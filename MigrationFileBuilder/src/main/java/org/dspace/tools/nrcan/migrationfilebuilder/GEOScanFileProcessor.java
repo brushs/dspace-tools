a891d8308408ea4213886f5a9d4c714cadb334e5
steve.brush@apption.com <steve.brush@apption.com>
2023-02-09 09:39:03 -0500

    Added fields, relationships

--- MigrationFileBuilder/src/main/java/org/dspace/tools/nrcan/migrationfilebuilder/GEOScanFileProcessor.java
+++ MigrationFileBuilder/src/main/java/org/dspace/tools/nrcan/migrationfilebuilder/GEOScanFileProcessor.java
@@ -7,6 +7,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.lang3.StringUtils;
@@ -23,12 +25,31 @@ public class GEOScanFileProcessor implements MetadataFileProcessor {
 	private String currentArchivePath;
 	private String currentItemPath;
 	private boolean filesOpen = false;
+	private boolean beforeFirstItem = true;
 	private PrintStream contentsFileStream;
 	private PrintStream relationshipsFileStream;
 	private PrintStream dublinCoreFileStream;
 	private PrintStream dspaceFileStream;
 	private PrintStream nrcanFileStream;
+	private Map<String, String> dcElementTemplates;
+	private Map<String, String> nrcanElementTemplates;
+	private Map<String, Relationship> relationshipElements;
 
+	private static final String VALUE = "##VALUE##";
+	private static final String LANGUAGE = "##LANG##";
+	
+	private static final String ELEMENT_CONTENT = "dc:content";
+	private static final String ELEMENT_TITLE = "dc:titlea";
+	private static final String ELEMENT_VOLUME = "volume";
+	private static final String ELEMENT_SERIAL = "serialtitle";
+	private static final String ELEMENT_AUTHOR = "dc:creator";
+	
+	private static final String RELATIONSHIP_SERIAL = "isSerialOfPublication";
+	private static final String RELATIONSHIP_AUTHOR = "isAuthorOfPublication";
+	
+	private static final String ATTRIBUTE_TITLE = "dc.title";
+	private static final String ATTRIBUTE_MIGRATION_ID = "nrcan.author.migrationid";
+	
 	public GEOScanFileProcessor(String inPath, String outPath, CommandLine cmd) {
 		this.inPath = inPath;
 		this.outPath = outPath;
@@ -36,6 +57,8 @@ public class GEOScanFileProcessor implements MetadataFileProcessor {
 	
 	public void process() {
 		try {
+			initializeElementTemplates();
+			
 			inputStream = new FileInputStream(inPath);
 			streamReader = new BufferedReader(new InputStreamReader(inputStream));
 	
@@ -46,7 +69,8 @@ public class GEOScanFileProcessor implements MetadataFileProcessor {
 				line = streamReader.readLine();
 			}
 		}
-		catch(IOException ex) {
+		catch(Exception ex) {
+			System.out.println(ex);
 			throw new RuntimeException(ex.getMessage(), ex);
 		}
 		finally {
@@ -78,6 +102,10 @@ public class GEOScanFileProcessor implements MetadataFileProcessor {
 	}
 	
 	public void processLine(String input) {
+		if (StringUtils.isEmpty(input.trim())) {
+			return;
+		}
+		
 		if (StringUtils.trim(input).equals("<item>")) {
 			if (itemCount == archiveSize || StringUtils.isEmpty(currentArchivePath)) {
 				currentArchivePath = "archive_" + String.format("%03d" , archiveCount++);
@@ -88,17 +116,115 @@ public class GEOScanFileProcessor implements MetadataFileProcessor {
 			
 			String path = outPath + "\\" + currentArchivePath + "\\" + currentItemPath;
 			//System.out.println("PATH: " + path);
+			
 			createDirectory(path);
 			openNewOutputFiles(path);
 			filesOpen = true;
+			beforeFirstItem = false;
+			return;
 		}
 		
-		if (StringUtils.trim(input).equals("</item>")) {
+		if (StringUtils.trim(input).substring(0, Math.min(7, input.length())).equals("</item>")) {
 			closeOutputFiles();
 			filesOpen = false;
+			return;
 		}
+		
+		if (!beforeFirstItem) {
+			processMetadata(input);
+		}	
 	}
 
+	private void processMetadata(String line) {
+		Integer indexOfGT = line.indexOf(">");
+		Integer indexOfSpace = line.indexOf(" ");
+		
+		String element = line.substring(1, 
+				Math.min(indexOfGT < 0 ? line.length()-1 : indexOfGT, indexOfSpace < 0 ? line.length()-1 : indexOfSpace));
+		
+		processElement(element.toLowerCase(), line);
+	}
+	
+	private void processElement(String element, String line) {
+		
+		if (element.contentEquals(ELEMENT_CONTENT)) {
+			processContent(element, line);
+			return;
+		}
+		
+		if (relationshipElements.containsKey(element)) {
+			processRelationship(element, line);
+			return;
+		}
+		
+		boolean isDCElement = true;
+		String template = dcElementTemplates.get(element);
+		if (StringUtils.isEmpty(template)) {
+			isDCElement = false;
+			template = nrcanElementTemplates.get(element);
+		}
+		
+		if (StringUtils.isEmpty(template)) {
+			System.out.println("UNKNOWN ELEMENT: " + element);
+			return;
+		}
+		
+		String value = "";
+		String language = "";
+		
+		switch (element) {
+			case ELEMENT_TITLE :
+				value = getElementGeneric(line);
+				language = getElementLanguageGeneric(line);
+				break;
+			case ELEMENT_VOLUME :
+				value = getElementGeneric(line);
+				break;
+			default :
+				//
+		};
+		
+		template = template.replace(VALUE, value);
+		template = template.replace(LANGUAGE, language);
+		
+		if (isDCElement) {
+			dublinCoreFileStream.println(template);
+		} else {
+			nrcanFileStream.println(template);
+		}
+		
+	}
+	
+	private void processContent(String element, String line) {
+		
+		String value = getElementGeneric(line);
+
+		String output = "-r -s 0 -f " + value;
+		
+		contentsFileStream.println(output);
+	}
+	
+	private void processRelationship(String element, String line) {
+		
+		String value = "";
+		switch (element) {
+		case ELEMENT_SERIAL :
+			value = getElementGeneric(line);
+			break;
+		case ELEMENT_AUTHOR :
+			value = getAuthorMigrationId(line);
+			break;
+		default :
+			//
+		};
+		
+		Relationship rel = relationshipElements.get(element);
+		
+		String output = "relationship." + rel.getName() + " " + rel.getAttribute() + ":" + value;
+		
+		relationshipsFileStream.println(output);
+	}
+	
 	private void createDirectory(String path) {
 		File theDir = new File(path);
 		if (!theDir.exists()){
@@ -114,9 +240,14 @@ public class GEOScanFileProcessor implements MetadataFileProcessor {
 		nrcanFileStream = getPrintStream(path + "\\metadata_nrcan.xml");
 		
 		initializeDSpaceFile();
+		initializeDublinCoreFile();
+		initializeNRCanFile();
 	}
 	
 	private void closeOutputFiles() {
+		finalizeDublinCoreFile();
+		finalizeNRCanFile();
+		
 		contentsFileStream.close();
 		relationshipsFileStream.close();
 		dublinCoreFileStream.close();
@@ -128,5 +259,73 @@ public class GEOScanFileProcessor implements MetadataFileProcessor {
 		dspaceFileStream.println("<dublin_core schema=\"dspace\">");
 		dspaceFileStream.println("<dcvalue element=\"entity\" qualifier=\"type\">Publication</dcvalue>");
 		dspaceFileStream.println("</dublin_core>");
+	}
+	
+	private void initializeDublinCoreFile() {
+		dublinCoreFileStream.println("<dublin_core>");
+	}
+	
+	private void finalizeDublinCoreFile() {
+		dublinCoreFileStream.println("</dublin_core>");
+	}
+	
+	private void initializeNRCanFile() {
+		nrcanFileStream.println("<dublin_core schema=\"nrcan\">");
+	}
+	
+	private void finalizeNRCanFile() {
+		nrcanFileStream.println("</dublin_core>");
+	}
+	
+	private void initializeElementTemplates() {
+		dcElementTemplates = new HashMap<String, String>();
+		
+		//dcElementTemplates.put(ELEMENT_TITLE, "<dcvalue element=\"title\" qualifier=\"alternative\" language=\"" + "##LANG##" + "\">" + VALUE + "</dcvalue>");
+		dcElementTemplates.put(ELEMENT_TITLE, "<dcvalue element=\"title\" language=\"" + "##LANG##" + "\">" + VALUE + "</dcvalue>");
+		
+		nrcanElementTemplates = new HashMap<String, String>();
+		
+		nrcanElementTemplates.put(ELEMENT_VOLUME, "<dcvalue element=\"volume\" qualifier=\"\">" + VALUE + "</dcvalue>");
+		
+		relationshipElements = new HashMap<String, Relationship>();
+		
+		relationshipElements.put(ELEMENT_SERIAL, new Relationship(RELATIONSHIP_SERIAL, ATTRIBUTE_TITLE));
+		relationshipElements.put(ELEMENT_AUTHOR, new Relationship(RELATIONSHIP_AUTHOR, ATTRIBUTE_MIGRATION_ID));
+		
+	}
+	
+	private String getElementGeneric(String line) {
+		return line.substring(line.indexOf(">") + 1, line.substring(1).indexOf("<") + 1);
+	}
+	
+	private String getElementLanguageGeneric(String line) {
+		int pos = line.indexOf("xml:lang");
+		return line.substring(pos + 10, pos + 12);
+	}
+	
+	private String getAuthorMigrationId(String line) {
+		try {
+			int pos = line.indexOf("given_name");
+			String firstName = line.substring(pos + 11, line.indexOf("</given_name"));
+			pos = line.indexOf("surname");
+			String lastName = line.substring(pos + 8, line.indexOf("</surname"));
+			String deptId = "";
+			try {
+				pos = line.indexOf("dpsid");
+				deptId = line.substring(pos + 6, line.indexOf("</dpsid"));
+			} catch (Exception e) {
+				//
+			}
+			String orcId = "";
+			try {
+				pos = line.indexOf("ORCID");
+				orcId = line.substring(pos + 44, line.indexOf("</ORCID"));
+			} catch (Exception e) {
+				//
+			}
+			return firstName + "_" + lastName + "_" + deptId + "_" + orcId;
+		} catch (Exception e) {
+			return null;
+		}		
 	}
 }
---
+++ MigrationFileBuilder/src/main/java/org/dspace/tools/nrcan/migrationfilebuilder/Relationship.java
@@ -0,0 +1,25 @@
+package org.dspace.tools.nrcan.migrationfilebuilder;
+
+public class Relationship {
+
+	private String name;
+	private String attribute;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAttribute() {
+		return attribute;
+	}
+	public void setAttribute(String attribute) {
+		this.attribute = attribute;
+	}
+	
+	public Relationship(String name, String attribute) {
+		this.name = name;
+		this.attribute = attribute;
+	}
+}
